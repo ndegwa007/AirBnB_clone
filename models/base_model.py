f6a198a6c0e8cd0e80f4be5a048640895165c477
ndegz <ndegz@example.com>
2023-02-09 21:34:21 -0800

    store first object

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ class base model
 
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -40,7 +41,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # models.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -58,7 +59,7 @@ class BaseModel:
         attribute updated_at with current datetime
         """
         self.updated_at = datetime.now()
-        # models.storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
