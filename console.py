35bf4e5a31188aa22d9aa2f0582af7e8d6778330
ndegz <ndegz@example.com>
2023-02-14 08:34:38 -0800

    updates for all classes

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ from models.place import Place
 from models.state import State
 from models.amenity import Amenity
 from models.review import Review
+from models.city import City
 
 classes = {
         "BaseModel": BaseModel,
