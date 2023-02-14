bf3cca2db7d80dc68d96c052d40f7081c40b3855
ndegz <ndegz@example.com>
2023-02-14 07:43:06 -0800

    tests&files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -186,25 +186,6 @@ class TestBaseModel_to_dict(unittest.TestCase):
         with self.assertRaises(TypeError):
             bm.to_dict(None)
 
-class TestFileStorage_instantiation(unittest.TestCase):
-    """ Unittest for testing instantiation of the FileStorage class. """
-
-    def test_FileStorage_instantiation_no_args(self):
-        self.assertEqual(type(FileStorage()), FileStorage)
-
-    def test_FileStorag_instantiation_with_args(self):
-        with self.assertRaises(TypeError):
-            FileStorage(None)
-
-    def test_FileStorage_file_path_is_private_str(self):
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
-
-    def testFileStorage_objects_is_private_dict(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-    def test_storage_initializes(self):
-        self.assertEqual(type(models.storage), FileStorage)
-
 
 if __name__ == "__main__":
     unittest.main()
