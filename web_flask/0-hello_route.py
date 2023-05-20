deda8973c26e9f54d981ab23bace9d9b4ab0254b
ndegz <ndegwa8781@gmail.com>
2023-05-20 15:27:43 +0300

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-#a minimal flask app
+# a minimal flask app
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """ display hello HBNB """
+
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
