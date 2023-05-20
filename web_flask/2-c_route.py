9a644a59604ea4d1d00e94d31b5ed2976089a541
ndegz <ndegwa8781@gmail.com>
2023-05-20 15:21:28 +0300

    flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#a minimal flask app
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ display hello HBNB """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ display HBNB """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ c is _ """
+    modified_text = text.replace('_', ' ')
+    return "C %s" % modified_text
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+#a minimal flask app
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ display hello HBNB """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ display HBNB """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ c is _ """
+    modified_text = text.replace('_', ' ')
+    return "C %s" % modified_text
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text=None):
+    """ python text """
+    if text is None or text == '':
+        text = 'is cool'
+    else:
+        text = text.replace('_', ' ')
+
+    return "Python %s" % text
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numbers(n):
+    """numbers only"""
+    return "%d is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
