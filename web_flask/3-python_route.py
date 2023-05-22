b5c62da40117faac4415011052d4a5af5ce62cf6
ndegz <ndegwa8781@gmail.com>
2023-05-22 09:44:25 +0300

    flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """a minimal flask app"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """ display hello HBNB """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """ display HBNB """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ c is _ """
     modified_text = text.replace('_', ' ')
     return "C %s" % modified_text
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -30,6 +33,7 @@ def python_text(text=None):
         text = text.replace('_', ' ')
 
     return "Python %s" % text
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
