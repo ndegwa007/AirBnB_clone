3477d54ac928693115847d0df8eb862e70eaaf2f
ndegz <ndegwa8781@gmail.com>
2023-05-24 08:13:32 +0300

    templates

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,5 +46,10 @@ def number_temp(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def oddOrEven(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
